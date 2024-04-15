404f36bb7fe6039ef5bf5209c80bed278f8f2560
josephDev <ikechukwujoseph146@gmail.com>
2024-04-15 15:00:41 +0100

    creATed search Api Route

--- api/Routes/listing.route.js
+++ api/Routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, getListing } from '../Controllers/listing.controller.js';
+import { createListing, getListing, getListings } from '../Controllers/listing.controller.js';
 import { protect, authorize } from '../Middleware/Auth.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post("/create", verifyToken, protect,authorize('admin'), createListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 export default router;
